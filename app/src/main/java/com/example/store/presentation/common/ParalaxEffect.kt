50c0d5fa6fba45c5d289d202bb9cde6994b7f83d
Kahilo Massango <kahilo42@gmail.com>
2024-06-05 19:22:43 +0100

    adding screens

---
+++ app/src/main/java/com/example/store/presentation/common/ParalaxEffect.kt
@@ -0,0 +1,15 @@
+package com.example.store.presentation.common
+
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.layout
+
+fun Modifier.parallaxLayoutModifier(scrollState: ScrollState, rate: Int) =
+    layout { measurable, constraints ->
+        val placeable = measurable.measure(constraints)
+        val height = if (rate > 0) scrollState.value / rate else scrollState.value
+        layout(placeable.width, placeable.height) {
+            placeable.place(0, height)
+        }
+    }
