c4c928b245fe883c9d67ba7e5125369ca4064ef3
Kahilo Massango <kahilo42@gmail.com>
2024-06-08 08:31:24 +0100

    chore: add ThemePreviews

--- app/src/main/java/com/example/store/presentation/screens/shop/component/ProductGrid.kt
+++ app/src/main/java/com/example/store/presentation/screens/shop/component/ProductGrid.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.store.presentation.component.ProductCard
+import com.example.store.presentation.component.ThemePreviews
 import com.example.store.ui.theme.StoreTheme
 import kotlinx.coroutines.launch
 
@@ -95,7 +96,7 @@ fun ProductGrid(
     }
 }
 
-@Preview
+@ThemePreviews
 @Composable
 private fun Preview() {
     StoreTheme {
