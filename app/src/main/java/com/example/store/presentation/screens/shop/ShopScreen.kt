ff04bda0d3ccfa3ae2a5bb21692d8c9531e9aa06
Kahilo Massango <kahilo42@gmail.com>
2024-06-08 08:42:15 +0100

    refactor: add ShopScreenUiState

--- app/src/main/java/com/example/store/presentation/screens/shop/ShopScreen.kt
+++ app/src/main/java/com/example/store/presentation/screens/shop/ShopScreen.kt
@@ -3,8 +3,10 @@ package com.example.store.presentation.screens.shop
 import android.util.Log
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
@@ -19,40 +21,43 @@ import com.example.store.presentation.screens.shop.model.ShopSection
 import com.example.store.presentation.screens.shop.model.getCategorySectionFilters
 import com.example.store.ui.theme.StoreTheme
 
+data class ShopScreenUiState(
+    val currentContent: ShopSection = ShopSection.Categories,
+    val categorySection: CategorySection = CategorySection.Women,
+    val selectedCategory: String = ""
+
+)
+
 @Composable
 fun ShopScreen(
     navController: NavController,
     modifier: Modifier = Modifier,
 ) {
-    // State to keep track of the current content being displayed on the ShopScreen
-    var currentShopContent by rememberSaveable { mutableStateOf(ShopSection.Products) }
+    var uiState by remember { mutableStateOf(ShopScreenUiState()) }
 
-    // State to save the current category section when navigating back from ShopCategories
-    var currentCategorySection by rememberSaveable { mutableStateOf(CategorySection.Women) }
-    var selectedCategory by rememberSaveable { mutableStateOf("") }
     AnimatedContent(
         modifier = modifier,
-        targetState = currentShopContent,
+        targetState = uiState.currentContent,
         label = "AnimatedContent"
     ) { section ->
         when(section) {
             ShopSection.Products -> ProductListingScreen(
-                productSection = currentCategorySection,
-                productCategory = selectedCategory,
-                filterList= getCategorySectionFilters(currentCategorySection),
+                productSection = uiState.categorySection,
+                productCategory = uiState.selectedCategory,
+                filterList= getCategorySectionFilters(uiState.categorySection),
                 onFilterChange = { /*TODO: Implement filter change */ },
                 onProductClick = {
                     navController.navigate(Screen.ProductDetail)
                 },
-                onNavigateUp = { currentShopContent = ShopSection.Categories }
+                onNavigateUp = { uiState = uiState.copy(currentContent = ShopSection.Categories)  }
             )
             ShopSection.Categories -> CategorySelectionScreen(
-                currentSection = currentCategorySection,
-                onSectionClick = { currentCategorySection = it },
+                currentSection = uiState.categorySection,
+                onSectionClick = { uiState = uiState.copy(categorySection = it) },
                 onCategoryClick = { category ->
-                    selectedCategory = category
-                    currentShopContent = ShopSection.Products
-                    Log.d("ShopScreen", "${currentCategorySection.name} -> $category")
+                    uiState = uiState.copy(selectedCategory = category)
+                    uiState = uiState.copy(currentContent = ShopSection.Products)
+                    Log.d("ShopScreen", "${uiState.categorySection.name} -> $category")
                 }
             )
         }
