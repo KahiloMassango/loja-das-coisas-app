7d4d0b114e1718b7b4751d00506b1f856127d524
Kahilo Massango <kahilo42@gmail.com>
2024-07-08 04:56:23 +0100

    refactor: replace OutlinedTextField by BasicTextField and add placeholder animation

--- app/src/main/java/com/example/store/presentation/component/StoreTextField.kt
+++ app/src/main/java/com/example/store/presentation/component/StoreTextField.kt
@@ -1,12 +1,22 @@
 package com.example.store.presentation.component
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -14,15 +24,19 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.example.store.ui.theme.StoreTheme
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun StoreTextField(
     modifier: Modifier = Modifier,
     value: String,
     placeholder: String,
+    elevation: Dp = 2.dp,
     isError: Boolean = false,
     enabled: Boolean = true,
     errorMessage: String = "",
@@ -32,50 +46,73 @@ fun StoreTextField(
     visualTransformation: VisualTransformation = VisualTransformation.None,
 ) {
     val textFieldBorderColor = if (isError) MaterialTheme.colorScheme.error
-    else MaterialTheme.colorScheme.background
+    else Color.Transparent
 
-    OutlinedTextField(
-        modifier = modifier.clip(MaterialTheme.shapes.medium),
-        value = value,
-        onValueChange = { onValueChange(it) },
-        shape = MaterialTheme.shapes.medium,
-        singleLine = true,
-        enabled = enabled,
-        textStyle = MaterialTheme.typography.bodyMedium,
-        placeholder = {
-            Text(
-                text = placeholder,
-                style = MaterialTheme.typography.bodySmall,
-                color = if (isError) MaterialTheme.colorScheme.error
-                else MaterialTheme.colorScheme.onSurfaceVariant
-            )
-        },
-        supportingText = {
-            if (isError) {
-                Text(
-                    text = errorMessage,
-                    style = MaterialTheme.typography.labelSmall,
-                    color = MaterialTheme.colorScheme.error)
+    Column(
+        modifier = modifier
+
+    ) {
+        Surface(
+            modifier = modifier.height(60.dp),
+            shadowElevation = if (isError) 0.dp else elevation,
+            shape = MaterialTheme.shapes.small,
+            color = MaterialTheme.colorScheme.background,
+            border = BorderStroke(1.dp, textFieldBorderColor)
+        ) {
+            Column(
+                modifier = Modifier.padding(12.dp), verticalArrangement = Arrangement.Center
+            ) {
+                AnimatedVisibility(
+                    visible = value.isNotEmpty(),
+                    enter = slideInVertically(initialOffsetY = { it }),
+                    exit = slideOutVertically(targetOffsetY = { it })
+                ) {
+                    Text(
+                        modifier = Modifier.padding(bottom = 3.dp),
+                        text = placeholder,
+                        style = MaterialTheme.typography.bodySmall,
+                        color = if (isError) MaterialTheme.colorScheme.error
+                        else MaterialTheme.colorScheme.onSurfaceVariant
+                    )
+                }
+                BasicTextField(
+                    value = value,
+                    onValueChange = { onValueChange(it) },
+                    enabled = enabled,
+                    textStyle = MaterialTheme.typography.bodyMedium.copy(color = MaterialTheme.colorScheme.onBackground),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(MaterialTheme.shapes.medium),
+                    decorationBox = { innerTextField ->
+                        Box(
+                            modifier = Modifier.background(MaterialTheme.colorScheme.background)
+                        ) {
+                            if (value.isEmpty()) {
+                                Text(
+                                    text = placeholder,
+                                    style = MaterialTheme.typography.bodyMedium.copy(color = MaterialTheme.colorScheme.inverseOnSurface)
+                                )
+                            }
+                            innerTextField()
+                        }
+                    },
+                    cursorBrush = SolidColor(MaterialTheme.colorScheme.primary),
+                    keyboardOptions = keyboardOptions,
+                    keyboardActions = keyboardActions,
+                    visualTransformation = visualTransformation
+                )
+
             }
-        },
-        isError = isError,
-        colors = TextFieldDefaults.outlinedTextFieldColors(
-            containerColor = MaterialTheme.colorScheme.background,
-            focusedBorderColor = textFieldBorderColor,
-            unfocusedBorderColor = textFieldBorderColor,
-            focusedTextColor = MaterialTheme.colorScheme.onBackground,
-            unfocusedTextColor = MaterialTheme.colorScheme.onBackground,
-            errorTextColor = MaterialTheme.colorScheme.onBackground,
-            errorContainerColor = MaterialTheme.colorScheme.background,
-            disabledTextColor = MaterialTheme.colorScheme.inverseOnSurface,
-            disabledLabelColor = MaterialTheme.colorScheme.inverseOnSurface,
-
-            ),
-        keyboardOptions = keyboardOptions,
-        keyboardActions = keyboardActions,
-        visualTransformation = visualTransformation
-    )
-
+        }
+        if (isError) {
+            Text(
+                modifier = Modifier.padding(top = 2.dp, start = 8.dp),
+                text = errorMessage,
+                style = MaterialTheme.typography.bodySmall,
+                color = MaterialTheme.colorScheme.error
+            )
+        }
+    }
 }
 
 
@@ -92,7 +129,6 @@ private fun Preview() {
             placeholder = "Password",
             isError = false,
             errorMessage = "Error Message"
-
         )
     }
 }
