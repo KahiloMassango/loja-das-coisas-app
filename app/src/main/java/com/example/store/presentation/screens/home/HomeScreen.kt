3515d8979fe98b363556c364e38dae2b9842999a
Kahilo Massango <kahilo42@gmail.com>
2024-07-14 17:05:39 +0100

    refactor: add home screen components

--- app/src/main/java/com/example/store/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/store/presentation/screens/home/HomeScreen.kt
@@ -2,17 +2,24 @@ package com.example.store.presentation.screens.home
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.example.store.navigation.Route
+import androidx.navigation.compose.rememberNavController
+import com.example.store.presentation.screens.home.component.HomeBanner
+import com.example.store.presentation.screens.home.component.Section
+import com.example.store.ui.theme.StoreTheme
 
 @Composable
 fun HomeScreen(
@@ -20,23 +27,61 @@ fun HomeScreen(
     modifier: Modifier = Modifier,
 ) {
     Surface(
-        modifier = modifier.padding()
+        modifier = modifier,
     ) {
         Column(
             modifier = Modifier
-                .padding(16.dp)
-                .fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState()),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "Home")
-            Button(
-                onClick = {
-                    navController.navigate(Route.AuthGraph)
-                }
-            ) {
+            HomeBanner()
+
+            Spacer(modifier = Modifier.height(18.dp))
+
+            HomeContent(Modifier)
+
+            Spacer(modifier = Modifier.height(18.dp))
+
+            /*Text(text = "Home")
+            Button(onClick = { navController.navigate(Route.AuthGraph) }) {
                 Text(text = "Auth Graph")
-            }
+            }*/
         }
+    }
+}
+
+@Composable
+fun HomeContent(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .padding(horizontal = 16.dp),
+        verticalArrangement = Arrangement.spacedBy(44.dp)
+    ) {
+        Section(
+            title = "Sale",
+            description = "Super summer sale",
+            items = 15,
+            onItemClick = {},
+            onViewAllClick = {}
+        )
+        Section(
+            title = "New",
+            description = "You’ve never seen it before!",
+            items = 15,
+            onItemClick = {},
+            onViewAllClick = {}
+        )
+
+    }
+}
+
+
+
+@PreviewLightDark
+@Composable
+private fun Preview() {
+    StoreTheme {
+        HomeScreen(navController = rememberNavController())
     }
 }
---
+++ app/src/main/java/com/example/store/presentation/screens/home/component/HomeSection.kt
@@ -0,0 +1,84 @@
+package com.example.store.presentation.screens.home.component
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import com.example.store.presentation.component.ProductCard
+import com.example.store.presentation.component.ProductCard1
+import com.example.store.ui.theme.StoreTheme
+
+@Composable
+fun Section(
+    modifier: Modifier = Modifier,
+    title: String,
+    description: String,
+    items: Int,
+    onItemClick: (Int) -> Unit,
+    onViewAllClick: () -> Unit
+) {
+    Column(
+        modifier = modifier.fillMaxWidth(),
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.headlineMedium,
+                color = MaterialTheme.colorScheme.onSurface,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                modifier = Modifier.clickable { onViewAllClick() },
+                text = "Ver tudo",
+                style = MaterialTheme.typography.labelSmall,
+                fontWeight = FontWeight.Medium,
+            )
+        }
+        Text(
+            text = description,
+            style = MaterialTheme.typography.labelMedium,
+            color = MaterialTheme.colorScheme.inverseOnSurface,
+            fontWeight = FontWeight.Medium
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        LazyRow(
+            modifier = Modifier,
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            item { ProductCard {} }
+            item { ProductCard {} }
+            item { ProductCard {} }
+            item { ProductCard {} }
+            item { ProductCard {} }
+            item { ProductCard {} }
+            item { ProductCard {} }
+
+        }
+    }
+}
+
+@PreviewLightDark
+@Composable
+private fun Preview() {
+    StoreTheme {
+        Section(title = "Sale", description = "Super summer sale", items = 15, onItemClick ={} ) {
+
+        }
+    }
+}
