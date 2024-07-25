fbb05cb68c52fb7d126e9a4206e7235e137e286d
Kahilo Massango <kahilo42@gmail.com>
2024-07-25 11:37:27 +0100

    refactor: remove FavoriteButton, Badge and add product model as parameter

--- app/src/main/java/com/example/store/presentation/component/ProductCard.kt
+++ app/src/main/java/com/example/store/presentation/component/ProductCard.kt
@@ -2,21 +2,19 @@ package com.example.store.presentation.component
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.outlined.FavoriteBorder
-import androidx.compose.material.icons.outlined.StarOutline
-import androidx.compose.material3.Badge
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -34,77 +32,61 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.store.R
+import com.example.store.core.data.mock.p1
+import com.example.store.core.model.Product
 import com.example.store.ui.theme.StoreTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProductCard(
     modifier: Modifier = Modifier,
-    onClick: () -> Unit,
+    product: Product,
+    onClick: (productId: String) -> Unit,
+    onFavoriteClick: (productId: String) -> Unit
 ) {
     Card(
-        modifier = modifier,
-        onClick = onClick,
+        modifier = modifier
+            .width(164.dp),
+        onClick = { onClick(product.id) },
         shape = RoundedCornerShape(topStart = 12.dp, topEnd = 12.dp),
         colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.surface
         )
     ) {
-        Box {
-            Column(
-                verticalArrangement = Arrangement.spacedBy(3.dp)
-            ) {
-                Image(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(184.dp)
-                        .clip(RoundedCornerShape(12.dp)),
-                    painter = painterResource(id = R.drawable.detail_image_ex2),
-                    contentDescription = null,
-                    contentScale = ContentScale.Crop
-                )
-                StarRating(
-                    modifier = Modifier.padding(vertical = 3.dp),
-                    totalRatings = 56,
-                    rating = 2
-                )
-                Text(
-                    text = "Dorothy Perkins",
-                    color = MaterialTheme.colorScheme.inverseOnSurface,
-                    style = MaterialTheme.typography.labelSmall,
-                    fontWeight = FontWeight.Light
-                )
-                Text(
-                    text = "T-Shirt SPANISH",
-                    color = MaterialTheme.colorScheme.onSurface,
-                    style = MaterialTheme.typography.bodySmall,
-                    fontWeight = FontWeight.SemiBold
-                )
-                Text(
-                    text = "2.000kz",
-                    color = MaterialTheme.colorScheme.onSurface,
-                    style = MaterialTheme.typography.labelMedium,
-                    fontWeight = FontWeight.SemiBold
-                )
-            }
-            Badge(
+        Column(
+            verticalArrangement = Arrangement.spacedBy(3.dp)
+        ) {
+            Image(
                 modifier = Modifier
-                    .align(Alignment.TopStart)
-                    .padding(4.dp),
-                contentColor = MaterialTheme.colorScheme.onPrimary,
-                containerColor = MaterialTheme.colorScheme.primary
-            ) {
-                Text(
-                    text = "-20%",
-                    style = MaterialTheme.typography.labelSmall
-                )
-            }
-            FavoriteButton(
-                modifier = Modifier
-                    .align(Alignment.BottomEnd)
-                    .padding(bottom = 55.dp),
-                isFavorite = false,
-                onClick = { /*TODO*/ }
+                    .fillMaxWidth()
+                    .height(184.dp)
+                    .clip(RoundedCornerShape(bottomStart = 12.dp, bottomEnd = 12.dp)),
+                painter = painterResource(id = R.drawable.men_clothes),
+                contentDescription = null,
+                contentScale = ContentScale.Crop
+            )
+            StarRating(
+                modifier = Modifier.padding(vertical = 3.dp),
+                totalRatings = product.totalRating,
+                rating = product.averageRating
+            )
+            Text(
+                text = product.storeName,
+                color = MaterialTheme.colorScheme.inverseOnSurface,
+                style = MaterialTheme.typography.labelSmall,
+                fontWeight = FontWeight.Light
+            )
+            Text(
+                text = product.name,
+                color = MaterialTheme.colorScheme.onSurface,
+                style = MaterialTheme.typography.bodySmall,
+                fontWeight = FontWeight.SemiBold
+            )
+            Text(
+                text = "${product.price}kz",
+                color = MaterialTheme.colorScheme.onSurface,
+                style = MaterialTheme.typography.labelMedium,
+                fontWeight = FontWeight.SemiBold
             )
         }
     }
@@ -122,15 +104,15 @@ fun FavoriteButton(
         shape = CircleShape,
         shadowElevation = 5.dp,
         color = if (isFavorite) MaterialTheme.colorScheme.primary
-            else MaterialTheme.colorScheme.surface,
+        else MaterialTheme.colorScheme.surface,
         contentColor = if (isFavorite) MaterialTheme.colorScheme.onPrimary
-            else MaterialTheme.colorScheme.inverseOnSurface
+        else MaterialTheme.colorScheme.inverseOnSurface
     ) {
         Icon(
             modifier = Modifier
                 .size(34.dp)
                 .padding(4.dp),
-            imageVector = if(isFavorite) Icons.Filled.Favorite else Icons.Outlined.FavoriteBorder,
+            imageVector = if (isFavorite) Icons.Filled.Favorite else Icons.Outlined.FavoriteBorder,
             contentDescription = null,
             //tint = MaterialTheme.colorScheme.inverseSurface
         )
@@ -145,7 +127,7 @@ fun StarRating(
 ) {
     Row(
         modifier = modifier,
-        verticalAlignment = Alignment.CenterVertically
+        verticalAlignment = Alignment.CenterVertically,
     ) {
 
         for (starIndex in 1..5) {
@@ -157,7 +139,7 @@ fun StarRating(
             Icon(
                 modifier = Modifier
                     .padding(end = 3.dp)
-                    .size(14.dp),
+                    .size(12.dp),
                 painter = painterResource(id = starIcon),
                 contentDescription = "Star $starIndex",
                 tint = starColor
@@ -176,12 +158,10 @@ fun StarRating(
 @Composable
 private fun Preview() {
     StoreTheme {
-        /*StarRate(
-            totalRatings = 15,
-            rating = 2
-        )*/
         ProductCard(
-            onClick = {}
+            onClick = {},
+            product = p1,
+            onFavoriteClick = {}
         )
     }
 }
