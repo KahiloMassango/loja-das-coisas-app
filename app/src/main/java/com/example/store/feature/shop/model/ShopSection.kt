b65d59d4eba2369ed555738f789d1914e8d9b8b1
Kahilo Massango <kahilo42@gmail.com>
2024-08-05 10:43:36 +0100

    refactor: simplify the ShopSection enum class

--- app/src/main/java/com/example/store/feature/shop/model/ShopSection.kt
+++ app/src/main/java/com/example/store/feature/shop/model/ShopSection.kt
@@ -1,15 +1,15 @@
 package com.example.store.feature.shop.model
 
 
-enum class ShopSection { Women, Men, Kids }
+enum class ShopSection(
+    val description: String
+) {
+    Women("Mulheres"),
+    Men("Homens"),
+    Kids("Crianças")
+}
 
-fun ShopSection.getSectionTitle(): String = sectionTitles[this]!!
 
-private val sectionTitles = mapOf(
-    ShopSection.Women to "Mulheres",
-    ShopSection.Men to "Homens",
-    ShopSection.Kids to "Crianças"
-)
 
 val shopSections = listOf(
     Pair(ShopSection.Women, "Mulheres"),
