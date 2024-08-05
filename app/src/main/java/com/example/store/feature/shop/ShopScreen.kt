cc3629d4a172fd6f51432c9da3e367881fbb189d
Kahilo Massango <kahilo42@gmail.com>
2024-08-05 10:40:03 +0100

    refactor: hoist ShopListingScreen states to  ShopScreen

--- app/src/main/java/com/example/store/feature/shop/ShopScreen.kt
+++ app/src/main/java/com/example/store/feature/shop/ShopScreen.kt
@@ -1,6 +1,5 @@
 package com.example.store.feature.shop
 
-import android.util.Log
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -9,13 +8,19 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.example.store.core.data.mock.productList
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.store.core.ui.ErrorScreen
+import com.example.store.core.ui.LoadingScreen
 import com.example.store.core.ui.component.ThemePreviews
 import com.example.store.core.ui.theme.StoreTheme
 import com.example.store.feature.shop.component.CategorySelectionScreen
-import com.example.store.feature.shop.component.ProductListingScreen
+import com.example.store.feature.shop.component.ShopListingScreen
+import com.example.store.feature.shop.component.SortOption
 import com.example.store.feature.shop.model.ShopScreenContent
 import com.example.store.feature.shop.model.ShopSection
+import com.example.store.feature.shop.model.kidsFilters
+import com.example.store.feature.shop.model.menFilters
+import com.example.store.feature.shop.model.womenFilters
 
 
 @Composable
@@ -25,9 +30,17 @@ fun ShopScreen(
     onProductClick: (String) -> Unit,
     onSearch: () -> Unit,
 ) {
+    val uiState = viewModel.uiState.collectAsStateWithLifecycle().value
     var currentContent by rememberSaveable { mutableStateOf(ShopScreenContent.Categories) }
     var currentSection by rememberSaveable { mutableStateOf(ShopSection.Women) }
     var currentCategory by rememberSaveable { mutableStateOf("") }
+    var orderBy by rememberSaveable { mutableStateOf(SortOption.Popular.title) }
+    val filters = when(currentSection){
+        ShopSection.Men -> menFilters
+        ShopSection.Women -> womenFilters
+        ShopSection.Kids -> kidsFilters
+    }
+    var filter by rememberSaveable { mutableStateOf(filters[0].name) }
 
 
     AnimatedContent(
@@ -36,28 +49,39 @@ fun ShopScreen(
         label = "AnimatedContent"
     ) { content ->
         when(content) {
-            ShopScreenContent.Products -> ProductListingScreen(
-                section = currentSection,
-                category = currentCategory,
-                products = productList,
-                onSearch = onSearch,
-                onFilterChange = { /*TODO: Implement filter change */ },
-                onProductClick = { onProductClick(it) },
-                onFavoriteClick = { /* TODO */ },
-                onNavigateUp = {
-                    currentContent = ShopScreenContent.Categories
-                }
-            )
             ShopScreenContent.Categories -> CategorySelectionScreen(
                 currentSection = currentSection,
                 onSearch = onSearch,
                 onSectionClick = { section -> currentSection = section },
                 onCategoryClick = { category ->
                     currentCategory = category
+                    viewModel.getProducts(currentSection.name, currentCategory, filter, orderBy)
                     currentContent = ShopScreenContent.Products
-                    Log.d("ShopScreen", "${currentSection.name} -> $category")
                 }
             )
+
+            ShopScreenContent.Products -> when(uiState) {
+                is ShopUiState.Loading -> LoadingScreen()
+                is ShopUiState.Error -> ErrorScreen(onTryAgain = {
+                    viewModel.getProducts(currentSection.name, currentCategory, filter, orderBy) }
+                )
+                is ShopUiState.Success -> ShopListingScreen(
+                    section = currentSection,
+                    category = currentCategory,
+                    currentFilter = filter,
+                    orderBy = orderBy,
+                    products = uiState.products,
+                    filters = filters,
+                    onFilterChange = { filter = it },
+                    onProductClick = { onProductClick(it) },
+                    onOrderBy = { orderBy = it },
+                    onSearch = onSearch,
+                    onNavigateUp = {
+                        currentContent = ShopScreenContent.Categories
+                    }
+                )
+            }
+
         }
     }
 }
--- app/src/main/java/com/example/store/feature/shop/model/ShopScreenUiState.kt
+++
@@ -1,7 +0,0 @@
-package com.example.store.feature.shop.model
-
-data class ShopScreenUiState(
-    val content: ShopScreenContent = ShopScreenContent.Categories,
-    val section: ShopSection = ShopSection.Women,
-    val category: String = ""
-)
