7e305a7433f75ffcd1199c6d5c6018be8dbf7eaf
Kahilo Massango <kahilo42@gmail.com>
2024-08-06 11:23:51 +0100

    refactor: add correct spacing between elements

--- app/src/main/java/com/example/store/feature/order_detail/OrderDetailScreen.kt
+++ app/src/main/java/com/example/store/feature/order_detail/OrderDetailScreen.kt
@@ -1,12 +1,12 @@
 package com.example.store.feature.order_detail
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
@@ -15,10 +15,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.store.core.ui.component.StoreCenteredTopBar
+import com.example.store.core.ui.theme.StoreTheme
 import com.example.store.feature.order_detail.components.DetailOrderProductCard
 import com.example.store.feature.order_detail.components.OrderInfo
 import com.example.store.feature.order_detail.components.OrderResume
-import com.example.store.core.ui.theme.StoreTheme
 
 @Composable
 fun OrderDetailScreen(
@@ -26,50 +26,42 @@ fun OrderDetailScreen(
     viewModel: OrderDetailViewModel = hiltViewModel(),
     onNavigateUp: () -> Unit
 ) {
-    val scrollState = rememberScrollState() // Hoist scroll state
 
     Scaffold(
         modifier = modifier,
         topBar = {
             StoreCenteredTopBar(
-                title = "Detalhes do Pedido", // Use string resources
+                title = "Detalhes do Pedido",
                 canNavigateBack = true,
-                onNavigateUp = onNavigateUp
-                // Elevation is usually handled by the theme
+                onNavigateUp = onNavigateUp,
+                elevation = 5.dp
             )
         }
     ) { contentPadding ->
         Surface(
-            modifier = Modifier
-                .padding(contentPadding)
-            //.verticalScroll(scrollState) // Use hoisted scroll state
+            modifier = Modifier.padding(contentPadding)
         ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(16.dp)
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                contentPadding = PaddingValues(16.dp),
+                verticalArrangement = Arrangement.spacedBy(18.dp)
             ) {
-                LazyColumn {
-                    item {
-                        OrderInfo(
-                            orderNumber = "1947034",
-                            date = "05-12-2019",
-                            itemsQty = 3,
-                            status = "Entregue"
-                        )
-                    }
-
-                    item { Spacer(modifier = Modifier.height(18.dp)) }
-
-                    items(7) {
-                        DetailOrderProductCard(modifier = Modifier.padding(bottom = 22.dp))
-                    }
-
-                    item { Spacer(modifier = Modifier.height(12.dp)) }
-
-                    item { OrderResume() }
-                    item { Spacer(modifier = Modifier.height(12.dp)) }
+                item {
+                    OrderInfo(
+                        orderNumber = "1947034",
+                        date = "05-12-2019",
+                        itemsQty = 3,
+                        status = "Entregue"
+                    )
                 }
+
+
+                items(7) {
+                    DetailOrderProductCard()
+                }
+
+                item { OrderResume() }
+
             }
         }
     }
