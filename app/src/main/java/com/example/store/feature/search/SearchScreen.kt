e72d4b42ccb73bfefef0fd8f3e0dbab94ea901b2
Kahilo Massango <kahilo42@gmail.com>
2024-08-12 15:21:20 +0100

    refactor: change search bar

--- app/src/main/java/com/example/store/feature/search/SearchScreen.kt
+++ app/src/main/java/com/example/store/feature/search/SearchScreen.kt
@@ -1,35 +1,30 @@
 package com.example.store.feature.search
 
-import android.text.style.SuggestionSpan
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBackIos
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.Divider
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.SearchBar
-import androidx.compose.material3.SearchBarDefaults
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
@@ -43,6 +38,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.store.core.ui.component.LargeProductCard
 import com.example.store.core.ui.theme.StoreTheme
+import com.example.store.feature.search.components.StoreSearchBar
 
 @Composable
 fun SearchScreen(
@@ -51,136 +47,96 @@ fun SearchScreen(
     onProductClick: (String) -> Unit,
     onNavigateUp: () -> Unit,
 ) {
-    val focusRequester = remember { FocusRequester() }
+    val searchFieldFocusRequester = remember { FocusRequester() }
     val keyboardManager = LocalSoftwareKeyboardController.current
     val focusManager = LocalFocusManager.current
-    var isSearchActive by remember { mutableStateOf(false) }
     val uiState by viewModel.searchResults.collectAsStateWithLifecycle()
     val searchText = viewModel.searchQuery
 
+
+    // Focus on search field when screen appears
     LaunchedEffect(Unit) {
-        focusRequester.requestFocus()
+        searchFieldFocusRequester.requestFocus()
     }
+
     Column(
         modifier = modifier
-            .fillMaxSize()
-            .pointerInput(Unit) {
-                detectTapGestures {
-                    focusManager.clearFocus()
-                }
-            },
+            .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        StoreSearchBar(
-            modifier = Modifier
-                .padding(top = 12.dp)
-                .focusRequester(focusRequester),
-            query = searchText,
-            onQueryChange = { viewModel.updateSearchQuery(it) },
-            onSearch = {
-                keyboardManager?.hide()
-                focusManager.clearFocus()
-            },
-            active = true,
-            onActiveChange = { isSearchActive = it },
-            onClearQuery = {
-                viewModel.updateSearchQuery("")
-                focusRequester.requestFocus()
-            },
-            onNavigateUp = {
-                onNavigateUp()
-            /* TODO */
-            }
+        Surface(
+            modifier = Modifier.fillMaxWidth(),
+            color = MaterialTheme.colorScheme.primary
         ) {
-            when {
-                uiState.isEmpty() and searchText.isNotEmpty() -> SearchEmptyState(message = "Nada Encontrado")
-                else /*uiState.isNotEmpty()*/ -> {
-                    LazyColumn(
-                        modifier = Modifier.fillMaxSize(),
-                        contentPadding = PaddingValues(16.dp),
-                        verticalArrangement = Arrangement.spacedBy(22.dp)
-                    ) {
-                        items(uiState) { product ->
-                            LargeProductCard(
-                                product = product,
-                                onClick = { onProductClick(it) }
-                            )
-                        }
+            Row(
+                modifier = Modifier
+                    .statusBarsPadding()
+                    .padding(10.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Icon(
+                    modifier = Modifier
+                        .size(24.dp)
+                        .clickable(
+                            interactionSource = null,
+                            indication = null
+                        ) { onNavigateUp() },
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.onPrimary
+                )
+                StoreSearchBar(
+                    modifier = Modifier
+                        .weight(1f)
+                        .focusRequester(searchFieldFocusRequester),
+                    query = searchText,
+                    onQueryChange = { viewModel.updateSearchQuery(it) },
+                    onSearch = {
+                        keyboardManager?.hide()
+                        focusManager.clearFocus()
+                    },
+                    onClearQuery = {
+                        viewModel.updateSearchQuery("")
+                    },
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        when {
+            uiState.isEmpty() and searchText.isNotEmpty() -> SearchEmptyState(
+                modifier = Modifier.pointerInput(Unit) {
+                    detectTapGestures { focusManager.clearFocus() }
+                },
+            )
+            else -> {
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .pointerInput(Unit) {
+                            detectTapGestures { focusManager.clearFocus() }
+                        },
+                    contentPadding = PaddingValues(16.dp),
+                    verticalArrangement = Arrangement.spacedBy(22.dp)
+                ) {
+                    items(uiState) { product ->
+                        LargeProductCard(
+                            product = product,
+                            onClick = { onProductClick(it) }
+                        )
                     }
                 }
             }
         }
-
     }
-
 }
 
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun StoreSearchBar(
+private fun SearchEmptyState(
     modifier: Modifier = Modifier,
-    query: String,
-    onQueryChange: (String) -> Unit,
-    onSearch: (String) -> Unit,
-    onActiveChange: (Boolean) -> Unit,
-    onNavigateUp: () -> Unit,
-    onClearQuery: () -> Unit,
-    active: Boolean,
-    content: @Composable () -> Unit
-) {
-    SearchBar(
-        query = query,
-        onQueryChange = { onQueryChange(it) },
-        onSearch = { onSearch(it) },
-        active = active,
-        onActiveChange = { onActiveChange(it) },
-        modifier = modifier,
-        placeholder = {
-            Text(text = "Search")
-        },
-        colors = SearchBarDefaults.colors(
-            containerColor = MaterialTheme.colorScheme.secondaryContainer,
-            inputFieldColors = TextFieldDefaults.colors(
-                focusedTextColor = MaterialTheme.colorScheme.onSecondaryContainer
-            )
-        ),
-        leadingIcon = {
-            if (active) {
-                IconButton(onClick = onNavigateUp) {
-                    Icon(
-                        imageVector = Icons.Default.ArrowBackIos,
-                        contentDescription = null,
-                        tint = MaterialTheme.colorScheme.inverseOnSurface
-                    )
-                }
-            } else {
-                Icon(
-                    imageVector = Icons.Default.Search,
-                    contentDescription = null,
-                    tint = MaterialTheme.colorScheme.inverseOnSurface
-                )
-            }
-        },
-        trailingIcon = {
-            if (query.isNotEmpty() && active) {
-                IconButton(onClick = onClearQuery) {
-                    Icon(
-                        imageVector = Icons.Default.Close,
-                        contentDescription = null,
-                        tint = MaterialTheme.colorScheme.inverseOnSurface
-                    )
-                }
-            }
-        },
-        content = { content() }
-    )
-}
-
-@Composable
-fun SearchEmptyState(
-    modifier: Modifier = Modifier,
-    message: String
 ) {
     Column(
         verticalArrangement = Arrangement.Center,
@@ -188,7 +144,7 @@ fun SearchEmptyState(
         modifier = modifier.fillMaxSize()
     ) {
         Text(
-            text = message,
+            text = "Nada Encontrado",
             style = MaterialTheme.typography.titleSmall
         )
         Text(
