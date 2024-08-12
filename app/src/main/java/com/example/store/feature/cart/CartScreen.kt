ece8c729e256cc6bf16886e71b58aded7fd06736
Kahilo Massango <kahilo42@gmail.com>
2024-08-12 10:39:40 +0100

    refactor: change image and text for empty cart screen

--- app/src/main/java/com/example/store/feature/cart/CartScreen.kt
+++ app/src/main/java/com/example/store/feature/cart/CartScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.exclude
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -29,6 +30,7 @@ import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -145,12 +147,20 @@ private fun EmptyCartScreen(modifier: Modifier = Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
+            modifier = Modifier.size(150.dp),
             painter = painterResource(id = R.drawable.empty_cart),
             contentDescription = null
         )
         Text(
-            text = "Carrinho vazio!",
-            style = MaterialTheme.typography.bodyMedium
+            modifier = Modifier.padding(bottom = 8.dp),
+            text = "Seu cesto está vazio",
+            style = MaterialTheme.typography.bodyMedium,
+            fontWeight = FontWeight.Medium
+        )
+        Text(
+            text = "Parece que você não adicionou nada ao seu cesto. Vá em frente e explore as principais categorias.",
+            style = MaterialTheme.typography.bodySmall,
+            textAlign = TextAlign.Center
         )
     }
 }
