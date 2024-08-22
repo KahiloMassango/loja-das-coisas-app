3daaaf179d288e912f46cd51815903098e71bdac
Kahilo Massango <kahilo42@gmail.com>
2024-08-22 08:39:56 +0100

    create QuestionCategorySelector

--- app/src/main/java/com/example/store/feature/help_center/HelpCenterScreen.kt
+++ app/src/main/java/com/example/store/feature/help_center/HelpCenterScreen.kt
@@ -6,11 +6,10 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -24,7 +23,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
@@ -44,6 +43,7 @@ fun HelpCenterScreen(
 
     Scaffold(
         modifier = modifier,
+        containerColor = MaterialTheme.colorScheme.surface,
         topBar = {
             StoreCenteredTopBar(
                 title = "Centro de ajuda",
@@ -70,45 +70,6 @@ fun HelpCenterScreen(
                 when (tab) {
                     0 -> FaqContent()
                     else -> ContactUsContent()
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun QuestionFilter(
-    modifier: Modifier = Modifier,
-    currentCategory: QuestionCategory,
-    onCategoryClick: (QuestionCategory) -> Unit
-) {
-    LazyRow(
-        modifier = modifier,
-        contentPadding = PaddingValues(horizontal = 16.dp),
-        horizontalArrangement = Arrangement.spacedBy(14.dp)
-    ) {
-        QuestionCategory.entries.forEach { category ->
-            val selected = currentCategory == category
-
-            item {
-                val backgroundColor = if (selected) MaterialTheme.colorScheme.primaryContainer else
-                    MaterialTheme.colorScheme.secondary
-                Box(
-                    modifier = Modifier
-                        .shadow(elevation = 4.dp, RoundedCornerShape(50))
-                        .clip(RoundedCornerShape(50))
-                        .background(backgroundColor, RoundedCornerShape(50))
-
-                        .clickable { onCategoryClick(category) }
-                ) {
-                    Text(
-                        modifier = Modifier.padding(10.dp),
-                        text = category.description,
-                        style = MaterialTheme.typography.bodySmall,
-                        fontWeight = FontWeight.Medium,
-                        color = if (selected) MaterialTheme.colorScheme.onPrimaryContainer
-                            else MaterialTheme.colorScheme.onSecondary,
-                    )
                 }
             }
         }
