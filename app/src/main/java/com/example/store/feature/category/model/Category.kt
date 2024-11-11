dbd1b66ee9a17f00eb8cf0a5c7b975ae47a7c32e
Kahilo Massango <kahilo42@gmail.com>
2024-11-11 15:50:31 +0100

    add cosmetic section

--- app/src/main/java/com/example/store/feature/category/model/Category.kt
+++ app/src/main/java/com/example/store/feature/category/model/Category.kt
@@ -13,16 +13,26 @@ data class Category(
 
 val womenCategories = listOf(
     Category(name = "Clothes", description = "Roupas", image = R.drawable.category),
-    Category(name = "Shoes", description = "Calçados", image = R.drawable.category)
+    Category(name = "Shoes", description = "Calçados", image = R.drawable.category),
+    Category(name = "Accessory", description = "Acessórios", image = R.drawable.category)
 )
 
 val menCategories = listOf(
     Category(name = "Clothes", description = "Roupas", image = R.drawable.men_clothes),
-    Category(name = "Shoes", description = "Calçados", image = R.drawable.men_shoes)
+    Category(name = "Shoes", description = "Calçados", image = R.drawable.men_shoes),
+    Category(name = "Accessory", description = "Acessórios", image = R.drawable.category)
 )
 
 val kidsCategories = listOf(
     Category(name = "Clothes", description = "Roupas", image = R.drawable.category),
-    Category(name = "Shoes", description = "Calçados", image = R.drawable.category)
+    Category(name = "Shoes", description = "Calçados", image = R.drawable.category),
+    Category(name = "Accessory", description = "Acessórios", image = R.drawable.category)
+)
+
+val cosmeticsCategories = listOf(
+    Category(name = "Skin Care", description = "Skin Care", image = R.drawable.category),
+    Category(name = "Makeup", description = "Makeup", image = R.drawable.category),
+    Category(name = "Hair care", description = "Cuidados com o cabelo", image = R.drawable.category),
+    Category(name = "Frangace", description = "Perfumes", image = R.drawable.category)
 )
 
