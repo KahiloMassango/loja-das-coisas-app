3841f550c514c3ec540dfd2692a3e7a0c6bf595f
Kahilo Massango <kahilo42@gmail.com>
2024-12-09 08:02:17 +0100

    fix background color

--- features/discover/src/main/java/com/example/store/features/discover/shop/component/ShopLoadingScreen.kt
+++ features/discover/src/main/java/com/example/store/features/discover/shop/component/ShopLoadingScreen.kt
@@ -1,11 +1,13 @@
 package com.example.store.features.discover.shop.component
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -17,7 +19,9 @@ internal fun ShopLoadingScreen(
     modifier: Modifier = Modifier
 ) {
     Column(
-        modifier = modifier.fillMaxSize()
+        modifier = modifier
+            .background(MaterialTheme.colorScheme.surface)
+            .fillMaxSize()
     ) {
         SortingHeader(
             modifier = Modifier
