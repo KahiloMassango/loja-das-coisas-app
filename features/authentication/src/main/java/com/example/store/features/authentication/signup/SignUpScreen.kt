3de87c2403e075c47e427ab40a89858811494102
Kahilo Massango <kahilo42@gmail.com>
2024-12-10 20:21:16 +0100

    refactor signup screen

--- features/authentication/src/main/java/com/example/store/features/authentication/signup/SignUpScreen.kt
+++ features/authentication/src/main/java/com/example/store/features/authentication/signup/SignUpScreen.kt
@@ -3,10 +3,8 @@ package com.example.store.features.authentication.signup
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
@@ -21,13 +19,11 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalFocusManager
@@ -36,9 +32,8 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
-import com.example.store.core.ui.R
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.store.core.ui.component.CustomButton
-import com.example.store.core.ui.component.SocialAuthButton
 import com.example.store.core.ui.component.StoreLargeTopBar
 import com.example.store.core.ui.component.StoreTextField
 import com.example.store.core.ui.component.ThemePreviews
@@ -48,7 +43,7 @@ import com.example.store.features.authentication.component.CustomClickableText
 @Composable
 fun SignUpScreen(
     modifier: Modifier = Modifier,
-    viewModel: SignUpViewModel = SignUpViewModel(),
+    viewModel: SignUpViewModel = hiltViewModel(),
     onLogin: () -> Unit,
     onNavigateUp: () -> Unit,
 ) {
